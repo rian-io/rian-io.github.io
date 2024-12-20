2c64d5c82382e4fb008ed2fedb79626a5ec9f0df
Rian de Oliveira <rian@Rians-MacBook-Air.local>
2024-12-20 01:14:22 -0300

    Start project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,38 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: false,
-  app: {
-    baseURL: process.env.NODE_ENV === "production" ? "/rian-io.hithub.io/" : "/",
-    buildAssetsDir: "assets",
-  },
   hooks: {
     'prerender:routes'({ routes }) {
       routes.clear() // Do not generate any routes (except the defaults)
     }
+  },
+  app: {
+    baseURL: process.env.NODE_ENV === "production" ? "/rian-io.hithub.io/" : "/",
+    buildAssetsDir: "assets",
+    head: {
+      titleTemplate: '%s - Rian Oliveira',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap' }
+      ]
+    },
+  },
+  css: [
+    '~/assets/styles/main.css' // Global CSS
+  ],
+  modules: [
+    '@nuxtjs/color-mode',
+  ],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'dark', // fallback value if not system preference found
   },
 })
