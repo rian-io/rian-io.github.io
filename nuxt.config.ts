2bf43f30b2e6466bd33bb3237242c134c88f18e2
Rian Oliveira <>
2024-12-21 14:29:53 -0300

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,6 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
-      base: { href: '/' },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
